bf916ffb4988487f1ee327700449013a9318d9b8
cecilia <e.oxypetalum@gmail.com>
2013-10-08 22:12:13 -0700

    added files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CharlieTest::Application.config.secret_token = 'bf1240570dd2462a08e9840bbc3149672fe35233594a0b0acf2f271edc4b11fe844794c54590c047787d3b1c5741ee343129d3a2ddd17547aa0062d91bff41af'
